fa421ee533fd83fcfc8d7a8b637bee9ab3603655
Tim <rtim10@ymail.com>
2023-09-13 17:07:58 -0400

    array automotive completed

--- factory.js
+++ factory.js
@@ -1,3 +1,17 @@
 class Factory{
-    
+    constructor(location, owner){
+        this.location = location
+        this.owner = owner
+        this.vehicles = []
 }
+createVehicle(vehicle){
+    this.vehicles.push(vehicle)
+}
+listVehicles(){
+    return this.vehicles
+}
+paintVehicle(vin, paintColor){
+    let vIndex = this.vehicles. findIndex(v => v.vin === vin)
+    this.vehicles[vIndex].paint(paintColor)
+}
+    }
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,18 @@
 class Vehicle {
-    
+    constructor(make, model, year, price, vin){
+        this.make = make
+        this.model = model
+        this.year = year
+        this.price = price
+        this.vin = vin
+        this.color = undefined
+    }
+    paint(color){
+        return this.color = color
+    }
+    drive(){
+        return this.year + " "+ this.make + " "+ this.model+ " drives"
+    }
 }
+
 
